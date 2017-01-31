10477b2e25c10794d39595059a7d07b2e9225600
Rodolfo Silva <rodolfo@grupomarinho.com.br>
2017-01-31 10:24:51 -0300

    Added arrow keys control
    Signed-off-by: Rodolfo Silva <rodolfo@grupomarinho.com.br>

--- js/snake.js
+++ js/snake.js
@@ -4,10 +4,15 @@ var unitSize = 10;
 var snakeHead;
 var xSpeed = unitSize;
 var ySpeed = 0;
+var left = 37;
+var right = 39;
+var up = 38;
+var down = 40;
 var food;
+var direction = "horizontal";
 
-function draw(piece){
-    context.fillStyle = "black";
+function draw(piece, style){
+    context.fillStyle = style;
     context.beginPath();
     context.rect(piece.x, piece.y, unitSize, unitSize);
     context.fill();
@@ -17,8 +22,10 @@ function updateGame(){
     context.clearRect(0, 0, canvas.width, canvas.height);
     snakeHead.x = snakeHead.x + xSpeed;
     snakeHead.y = snakeHead.y + ySpeed;
-    draw(snakeHead);
-    draw(food);
+    draw(snakeHead, "black");
+    draw(food, "red");
+    if (xSpeed != 0) direction = "horizontal";
+    if (ySpeed != 0) direction = "vertical";
 }
 
 function generateRandomPosition(){
@@ -37,15 +44,38 @@ function generateFoodPosition(){
     return newPos;
 }
 
+function handleKeyPress(evt){
+    if (evt.keyCode == left && direction == "vertical"){
+        xSpeed -= unitSize;
+        ySpeed = 0;
+    }
+    if (evt.keyCode == right && direction == "vertical"){
+        xSpeed += unitSize;
+        ySpeed = 0;
+    }
+    if (evt.keyCode == up && direction == "horizontal"){
+        xSpeed = 0;
+        ySpeed -= unitSize;
+    }
+    if (evt.keyCode == down && direction == "horizontal"){
+        xSpeed = 0;
+        ySpeed += unitSize;
+    }
+}
+
 function startGame(){
-    canvas = document.getElementById("mainCanvas");
-    context = canvas.getContext('2d');
-    
     snakeHead = {x:0, y:0};
-    draw(snakeHead);
+    draw(snakeHead, "black");
 
     food = generateFoodPosition();
-    draw(food);
+    draw(food, "red");
 
     setInterval(updateGame, 300);
 }
+
+window.onload = function(){
+    canvas = document.getElementById("mainCanvas");
+    context = canvas.getContext('2d');
+    startGame();
+    document.addEventListener('keydown', handleKeyPress);
+};
