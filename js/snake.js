c78ec3057e4f1a52efff21f1309dedea5c48fbea
Rodolfo Silva <rodolfo@grupomarinho.com.br>
2017-01-31 09:26:40 -0300

    Added motion to the snake
    Signed-off-by: Rodolfo Silva <rodolfo@grupomarinho.com.br>

--- js/snake.js
+++ js/snake.js
@@ -1,10 +1,28 @@
-function startGame(){
-    var canvas = document.getElementById("mainCanvas");
-    var context = canvas.getContext('2d');
-    
-    snakeHead = {x:0, y:0};
+var canvas;
+var context;
+var snakeHead;
+var xSpeed = 10;
+var ySpeed = 0;
+
+function draw(piece){
     context.fillStyle = "black";
     context.beginPath();
-    context.rect(snakeHead.x, snakeHead.y, 10, 10);
+    context.rect(piece.x, piece.y, 10, 10);
     context.fill();
+}
+
+function updateGame(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    snakeHead.x = snakeHead.x + xSpeed;
+    snakeHead.y = snakeHead.y + ySpeed;
+    draw(snakeHead);
+}
+
+function startGame(){
+    canvas = document.getElementById("mainCanvas");
+    context = canvas.getContext('2d');
+    
+    snakeHead = {x:0, y:0};
+    draw(snakeHead);
+    setInterval(updateGame, 300);
 }
